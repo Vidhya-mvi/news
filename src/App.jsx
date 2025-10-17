4ea1fa71d5e7bb88df431dde4244def670e4807b
vidhya.m <vidhyam.stackup@gmail.com>
2025-10-17 20:46:51 +0530

    news

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Search from "./pages/Search";
 import Favorites from "./pages/Favorites";
 
